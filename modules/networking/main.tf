59cf07121a648308c4714414273778fc732f0101
MarcoPolo483 <132707769+MarcoPolo483@users.noreply.github.com>
2025-11-13 11:15:19 -0500

    scaffold: modules\networking\main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,8 +1,9 @@
-﻿variable ""name_prefix"" { type = string }
-variable ""location""    { type = string }
-resource ""azurerm_resource_group"" ""rg"" {
-  name     = ""-rg-net""
+variable "name_prefix" { type = string }
+variable "location"    { type = string }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.name_prefix}-rg-net"
   location = var.location
 }
-output ""rg_name"" { value = azurerm_resource_group.rg.name }
 
+output "rg_name" { value = azurerm_resource_group.rg.name }
