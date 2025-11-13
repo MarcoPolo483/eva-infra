30ba76fac92c3aaef187a0305de9708557690b4c
MarcoPolo483 <132707769+MarcoPolo483@users.noreply.github.com>
2025-11-13 11:15:17 -0500

    scaffold: env\dev\backend.tf

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,9 +1,8 @@
-﻿terraform {
-  backend ""azurerm"" {
-    resource_group_name  = ""REPLACE-RG""
-    storage_account_name = ""replacestorageacct""
-    container_name       = ""tfstate""
-    key                  = ""eva-infra-dev.tfstate""
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "REPLACE-RG"
+    storage_account_name = "replacestorageacct"
+    container_name       = "tfstate"
+    key                  = "eva-infra-dev.tfstate"
   }
 }
-
