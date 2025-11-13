52385c2f1175b9fa03ec5359195bdcdb276de200
marco presta <marcopresta@yahoo.com>
2025-11-13 11:56:19 -0500

    Configure Azure backend and OIDC for Terraform

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "REPLACE-RG"
-    storage_account_name = "replacestorageacct"
+    resource_group_name  = "rg-evada2"
+    storage_account_name = "evatfstate123456"
     container_name       = "tfstate"
     key                  = "eva-infra-dev.tfstate"
   }
---
+++ env/dev/terraform.tfvars
@@ -0,0 +1,3 @@
+# Adjust to your preference; aligns with your East US region
+name_prefix = "eva-dev"
+location    = "eastus"
