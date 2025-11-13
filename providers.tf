d69afd6ee0d68e365ecc84d36ff8e394e6dff537
MarcoPolo483 <132707769+MarcoPolo483@users.noreply.github.com>
2025-11-13 08:38:53 -0500

    Update providers.tf to enforce required Terraform version and provider constraints

--- providers.tf
+++ providers.tf
@@ -1,17 +1,13 @@
-﻿terraform {
-  required_version = "">= 1.5.0""
+terraform {
+  required_version = ">= 1.5.0"
   required_providers {
     azurerm = {
-      source  = ""hashicorp/azurerm""
-      version = ""~> 4.0""
-    }
-    azapi = {
-      source  = ""azure/azapi""
-      version = ""~> 2.0""
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
   }
 }
 
-provider ""azurerm"" {
+provider "azurerm" {
   features {}
 }
