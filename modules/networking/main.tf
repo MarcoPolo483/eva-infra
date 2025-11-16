14c74fcf84f4da5a979b3c10ea9c210d3d176166
Marco Presta <132707769+MarcoPolo483@users.noreply.github.com>
2025-11-16 08:25:04 -0500

    scaffold: modules/networking/main.tf

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,16 +1,9 @@
-variable "name_prefix" {
-  type = string
-}
-
-variable "location" {
-  type = string
-}
+variable "name_prefix" { type = string }
+variable "location"    { type = string }
 
 resource "azurerm_resource_group" "rg" {
   name     = "${var.name_prefix}-rg-net"
   location = var.location
 }
 
-output "rg_name" {
-  value = azurerm_resource_group.rg.name
-}
+output "rg_name" { value = azurerm_resource_group.rg.name }
