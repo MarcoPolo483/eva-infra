55b2142e0e343208ffcf314c086b9d8010cc4d4b
Marco Presta <132707769+MarcoPolo483@users.noreply.github.com>
2025-11-16 08:25:02 -0500

    scaffold: env/dev/backend.tf

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-evada2"
-    storage_account_name = "evatfstate123456"
+    resource_group_name  = "REPLACE-RG"
+    storage_account_name = "replacestorageacct"
     container_name       = "tfstate"
     key                  = "eva-infra-dev.tfstate"
   }
