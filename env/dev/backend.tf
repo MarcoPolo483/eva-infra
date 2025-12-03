59aec65b98f1a76b0ca7ca9db8acf510072982e6
marco <marco@example.com>
2025-12-02 22:06:38 -0500

    update backend to use real storage account

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "REPLACE-RG"
-    storage_account_name = "replacestorageacct"
+    resource_group_name  = "eva-infra-tfstate-rg"
+    storage_account_name = "evainfratfstate"
     container_name       = "tfstate"
     key                  = "eva-infra-dev.tfstate"
   }
