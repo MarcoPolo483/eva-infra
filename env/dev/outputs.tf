9fddcb1c91dc8ce785a8b1248c6785cacf94db2d
marco <marco@example.com>
2025-12-02 22:09:18 -0500

    fix outputs to match networking module

--- env/dev/outputs.tf
+++ env/dev/outputs.tf
@@ -1,19 +1,4 @@
-output "vnet_id" {
-  description = "Virtual Network ID"
-  value       = module.networking.vnet_id
+output "resource_group_name" {
+  description = "Networking Resource Group Name"
+  value       = module.networking.rg_name
 }
-
-output "private_subnet_id" {
-  description = "Private Subnet ID"
-  value       = module.networking.private_subnet_id
-}
-
-output "public_subnet_id" {
-  description = "Public Subnet ID"
-  value       = module.networking.public_subnet_id
-}
-
-# Outputs for not-yet-implemented modules commented out
-# output "key_vault_id" - module.key_vault
-# output "monitoring" - module.monitoring
-# output "cosmos_db" - module.cosmos_db
