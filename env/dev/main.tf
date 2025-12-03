e069b9feb6ee4af96ed1b026fb097a77459aa8cb
marco <marco@example.com>
2025-12-02 22:08:06 -0500

    simplify to networking module only - others TBD

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -3,3 +3,8 @@ module "networking" {
   name_prefix = var.name_prefix
   location    = var.location
 }
+
+# Placeholder outputs - modules not yet implemented
+# module "key_vault" - To be added
+# module "monitoring" - To be added  
+# module "cosmos_db" - To be added
