2ae3d37571371afe03b45a4e90cee786e197dca4
Marco Presta <132707769+MarcoPolo483@users.noreply.github.com>
2025-12-07 22:14:32 -0500

    scaffold: env/dev/main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,10 +1,5 @@
 module "networking" {
-  source      = "../../modules/networking"
+  source      = "../modules/networking"
   name_prefix = var.name_prefix
   location    = var.location
 }
-
-# Placeholder outputs - modules not yet implemented
-# module "key_vault" - To be added
-# module "monitoring" - To be added  
-# module "cosmos_db" - To be added
